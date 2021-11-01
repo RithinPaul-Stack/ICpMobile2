42764e7dfb0067af0e2bbc04f1553f3fece87f37
RithinPaul-Stack <63901520+RithinPaul-Stack@users.noreply.github.com>
2021-11-01 00:47:11 -0500

    main

--- app/src/main/java/com/example/vijaya/myorder/OrderActivity.java
+++ app/src/main/java/com/example/vijaya/myorder/OrderActivity.java
@@ -37,8 +37,7 @@ public class OrderActivity extends AppCompatActivity {
 
         // Hint to accomplish the task
 
-
-
+/*
         Uri uri = Uri.parse("mailto:" + "tipparaju2010@gmail.com")
                 .buildUpon()
                 .appendQueryParameter("subject", "Order summary of "+name)
@@ -46,6 +45,28 @@ public class OrderActivity extends AppCompatActivity {
                 .build();
 
         Intent emailIntent = new Intent(Intent.ACTION_SENDTO, uri);
-        startActivity(Intent.createChooser(emailIntent, "chooserTitle"));
+        startActivity(Intent.createChooser(emailIntent, "chooserTitle"));*/
+
+        Intent email = new Intent(Intent.ACTION_SEND);
+        email.putExtra(Intent.EXTRA_EMAIL, new String[]{"tipparaju2010@gmail.com"});
+        email.putExtra(Intent.EXTRA_SUBJECT, "Order summary of "+name);
+        email.putExtra(Intent.EXTRA_TEXT, output);
+        email.setType("message/rfc822");
+        startActivity(Intent.createChooser(email, "Send Mail Using :"));
+
+
+            // Write the relevant code for triggering email
+
+            // Hint to accomplish the task
+/*
+            Intent intent= new Intent(Intent.ACTION_SENDTO);
+            intent.setType("text/plain");
+            intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"tipparaju2010@gmail.com"});
+            intent.putExtra(Intent.EXTRA_SUBJECT, "Order summary of "+name);
+            intent.putExtra(Intent.EXTRA_TEXT, output);
+
+            if(intent.resolveActivity(getPackageManager())!=null)
+                startActivity(Intent.createChooser(intent, "chooserTitle"));*/
+
     }
 }
